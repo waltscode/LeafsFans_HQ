a59ec21da048c58ab2d5b7c40d38cfd08c0bb817
waltscode <walterswork91@outlook.com>
2024-02-07 23:44:49 -0500

    added nodemon script and performed further unsuccessful testing

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -6,7 +6,9 @@ router.get('/', async (req, res) => {
     try {
         const blogData = await Blog.findAll();
 
+        // res.render('blogs', { blogs: blogData });
         res.json(blogData);
+        console.log(blogData);
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
